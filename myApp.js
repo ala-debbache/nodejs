e666c25a3c3777cd4a946241751d33484dcc5a36
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2021-09-02 14:05:30 +0100

    serving Hello Express

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Hello Express");
+});
+
+app.listen(port=3000,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
 
 
 
